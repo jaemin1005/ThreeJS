341bf62adfc76b238a3ae51eb8dd9e358284d9f8
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-18 19:25:13 +0900

    Modify | vite.config.ts
    @rolluo/plugin-babel을 추가하여, 바벨의 옵션을 추가하였다

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite';
+import { babel } from '@rollup/plugin-babel';
 export default defineConfig({
   server: {
     port: 3000,
   },
+
+  plugins : [
+    babel({
+      babelHelpers: 'runtime', // Babel 헬퍼 함수들을 런타임에 사용하도록 설정
+      extensions: ['.js', '.jsx', '.ts', '.tsx'], // Babel이 처리할 파일 확장자들
+      configFile: './babel.config.json', // Babel 설정 파일 경로
+    })
+  ],
+
   build: {
+    target: "es2015",
     outDir: 'vite',
     sourcemap: true,
   },
