b12bab1c0d6d609681e0a9209f0d26353ebeecb5
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-18 20:53:36 +0900

    Modify | vite.config.ts
    rollup-plugin-visualizer, 적용을 위핸 plugin 부분에 해당 옵션을 추가하였다

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import { babel } from '@rollup/plugin-babel';
+import { visualizer } from 'rollup-plugin-visualizer';
 export default defineConfig({
   server: {
     port: 3000,
@@ -10,7 +11,8 @@ export default defineConfig({
       babelHelpers: 'runtime', // Babel 헬퍼 함수들을 런타임에 사용하도록 설정
       extensions: ['.js', '.jsx', '.ts', '.tsx'], // Babel이 처리할 파일 확장자들
       configFile: './babel.config.json', // Babel 설정 파일 경로
-    })
+    }),
+    visualizer ({open : true})
   ],
 
   build: {
