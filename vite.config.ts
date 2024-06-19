ebe35632e58e8e92a42614d0d39f1aab9eefaf99
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-20 00:02:15 +0900

    Modify | vite.config.ts
    exclue: 'node_modules/**" 설정을 추가하여, 빌드 시간을 줄인다

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite';
 import { babel } from '@rollup/plugin-babel';
 import { visualizer } from 'rollup-plugin-visualizer';
+import resolve from '@rollup/plugin-node-resolve'; // 최신 플러그인 사용
+import commonjs from '@rollup/plugin-commonjs'; // 최신 플러그인 사용
 export default defineConfig({
   server: {
     port: 3000,
   },
 
   plugins : [
+    resolve(),
+    commonjs(),
     babel({
       babelHelpers: 'runtime', // Babel 헬퍼 함수들을 런타임에 사용하도록 설정
+      exclude: 'node_modules/**',
       extensions: ['.js', '.jsx', '.ts', '.tsx'], // Babel이 처리할 파일 확장자들
       configFile: './babel.config.json', // Babel 설정 파일 경로
     }),
