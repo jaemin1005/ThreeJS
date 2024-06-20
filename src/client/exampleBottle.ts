68be8c9f9c823566ddc5f5e4b2001d14c4f075c7
jaemin1005 <ghkdwoals1005@gmail.com>
2024-06-20 09:52:04 +0900

    New | exampleBottle
    Three 라이브러리 ocean example
    Typescript로 변환하였음

---
+++ src/client/exampleBottle.ts
@@ -0,0 +1,165 @@
+import * as THREE from 'three';
+import Stats from 'three/examples/jsm/libs/stats.module.js';
+import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { Water } from 'three/examples/jsm/objects/Water.js';
+import { Sky } from 'three/examples/jsm/objects/Sky.js';
+
+let container: HTMLElement | null;
+let stats: Stats;
+let camera: THREE.PerspectiveCamera;
+let scene: THREE.Scene;
+let renderer: THREE.WebGLRenderer;
+let controls: OrbitControls;
+let water: Water;
+let sun: THREE.Vector3;
+let mesh: THREE.Mesh;
+
+init();
+
+function init() {
+    container = document.getElementById('container');
+
+    if (!container) {
+        console.error("Container element not found");
+        return;
+    }
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setAnimationLoop(animate);
+    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    renderer.toneMappingExposure = 0.5;
+    container.appendChild(renderer.domElement);
+
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 1, 20000);
+    camera.position.set(30, 30, 100);
+
+    sun = new THREE.Vector3();
+
+    // Water
+    const waterGeometry = new THREE.PlaneGeometry(10000, 10000);
+
+    water = new Water(
+        waterGeometry,
+        {
+            textureWidth: 512,
+            textureHeight: 512,
+            waterNormals: new THREE.TextureLoader().load('resources/shaders/waternormals.jpg', (texture) => {
+                texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+            }),
+            sunDirection: new THREE.Vector3(),
+            sunColor: 0xffffff,
+            waterColor: 0x001e0f,
+            distortionScale: 3.7,
+            fog: scene.fog !== undefined
+        }
+    );
+
+    water.rotation.x = -Math.PI / 2;
+    scene.add(water);
+
+    // Skybox
+    const sky = new Sky();
+    sky.scale.setScalar(10000);
+    scene.add(sky);
+
+    const skyUniforms = sky.material.uniforms;
+    skyUniforms['turbidity'].value = 10;
+    skyUniforms['rayleigh'].value = 2;
+    skyUniforms['mieCoefficient'].value = 0.005;
+    skyUniforms['mieDirectionalG'].value = 0.8;
+
+    const parameters = {
+        elevation: 2,
+        azimuth: 180
+    };
+
+    const pmremGenerator = new THREE.PMREMGenerator(renderer);
+    const sceneEnv = new THREE.Scene();
+
+    let renderTarget: THREE.WebGLRenderTarget;
+
+    function updateSun() {
+        const phi = THREE.MathUtils.degToRad(90 - parameters.elevation);
+        const theta = THREE.MathUtils.degToRad(parameters.azimuth);
+
+        sun.setFromSphericalCoords(1, phi, theta);
+
+        sky.material.uniforms['sunPosition'].value.copy(sun);
+        water.material.uniforms['sunDirection'].value.copy(sun).normalize();
+
+        if (renderTarget !== undefined) renderTarget.dispose();
+
+        sceneEnv.add(sky);
+        renderTarget = pmremGenerator.fromScene(sceneEnv as any);
+        scene.add(sky);
+
+        scene.environment = renderTarget.texture;
+    }
+
+    updateSun();
+
+    // Box Mesh
+    const geometry = new THREE.BoxGeometry(30, 30, 30);
+    const material = new THREE.MeshStandardMaterial({ roughness: 0 });
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.maxPolarAngle = Math.PI * 0.495;
+    controls.target.set(0, 10, 0);
+    controls.minDistance = 40.0;
+    controls.maxDistance = 200.0;
+    controls.update();
+
+    stats = new Stats();
+    container.appendChild(stats.dom);
+
+    const gui = new GUI();
+
+    const folderSky = gui.addFolder('Sky');
+    folderSky.add(parameters, 'elevation', 0, 90, 0.1).onChange(updateSun);
+    folderSky.add(parameters, 'azimuth', -180, 180, 0.1).onChange(updateSun);
+    folderSky.open();
+
+    const waterUniforms = water.material.uniforms;
+
+    const folderWater = gui.addFolder('Water');
+    folderWater.add(waterUniforms.distortionScale, 'value', 0, 8, 0.1).name('distortionScale');
+    folderWater.add(waterUniforms.size, 'value', 0.1, 10, 0.1).name('size');
+    folderWater.open();
+
+    window.addEventListener('resize', onWindowResize);
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+    render();
+    stats.update();
+}
+
+function render() {
+    const time = performance.now() * 0.001;
+
+    if (mesh) {
+        mesh.position.y = Math.sin(time) * 20 + 5;
+        mesh.rotation.x = time * 0.5;
+        mesh.rotation.z = time * 0.51;
+    }
+
+    if (water) {
+        water.material.uniforms['time'].value += 1.0 / 60.0;
+    }
+
+    renderer.render(scene, camera);
+}
